49656c6a50fbea9173f4cf22d0250bf4fa2f717e
byonduniverse <masterjames318@gmail.com>
2023-05-30 06:29:44 -0400

    Resolved comments

--- components/Tutorial.vue
+++ components/Tutorial.vue
@@ -1,13 +1,20 @@
 <!-- Please remove this file from your project -->
 <template>
   <div>
-    <h1>Welcome to tutorial!</h1>
+    <h1 class="title">Welcome to tutorial!</h1>
     <p>You will learn Nuxt in details</p>
   </div>
 </template>
 
 <script>
 export default {
-  name: "NuxtTutorial",
+  name: 'NuxtTutorial',
 };
 </script>
+
+<style>
+.title {
+  font-weight: bold;
+  font-size: 24px;
+}
+</style>
